eaa68c2fa09a18eb7170b26b3538c60ebdc71b6f
Donovan Green <donovangreen360@gmail.com>
2022-07-30 23:47:36 -0400

    Readded Vehicle Class

---
+++ VehicleInventoryManagement/Vehicle.cpp
@@ -0,0 +1,59 @@
+#include "Vehicle.h"
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+// *********************
+//      CONSTRUCTOR
+// *********************
+
+Vehicle::Vehicle() {
+	manufacturer = "";
+	year = 2000;
+}
+
+// *******************************
+//      DISPLAY INFO FUNCTION
+// *******************************
+
+void Vehicle::displayInfo(Vehicle& vehicleRef) {
+	cout << "Vehicle Information: " << endl;
+	cout << "Manufacturer:        " << manufacturer << endl;
+	cout << "Year:                " << year << endl;
+}
+
+// ***********************
+//      SET FUNCTIONS
+// ***********************
+
+void Vehicle::setManufacturer(string newManu) {
+	manufacturer = newManu;
+}
+
+void Vehicle::setYear(int nYear) {
+	// Input validation to check if nYear is a 4 digit int.
+	if (nYear <= 999 || nYear > 9999) {
+		bool isValInput = false;
+		while (isValInput == false) {
+			cout << "Invalid year! please enter new year! ";
+			cin >> nYear;
+			if (nYear >= 1000 && nYear <= 9999) {
+				isValInput = true;
+			}
+		}
+	}
+	year = nYear;
+}
+
+// ***********************
+//      GET FUNCTIONS
+// ***********************
+
+string Vehicle::getManufacturer() {
+	return manufacturer;
+}
+
+int Vehicle::getYear() {
+	return year;
+}
+
+#endif
---
+++ VehicleInventoryManagement/Vehicle.h
@@ -0,0 +1,18 @@
+#pragma once
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Vehicle {
+protected:
+	string manufacturer;
+	int year;
+public:
+	Vehicle();
+	void setManufacturer(string);
+	void setYear(int);
+	string getManufacturer();
+	int getYear();
+	virtual void displayInfo(Vehicle&);
+};
